e643eac29a7b434c1c1001837094ecf2597e845d
Claudio-Nexal <claudio@nexal.it>
2025-12-17 11:08:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -565,31 +565,7 @@ if (!isFullwidth) {
 
 
 
-// Fade-in immagini con classe .fade-in (GSAP + ScrollTrigger)
-window.addEventListener("load", () => {
-  if (!window.gsap || !window.ScrollTrigger) return;
-  gsap.registerPlugin(ScrollTrigger);
 
-  gsap.utils.toArray(".immagine-fade-in").forEach((el) => {
-    // stato iniziale
-    gsap.set(el, { autoAlpha: 0, y: 16 });
-
-    gsap.to(el, {
-      autoAlpha: 1,
-      y: 0,
-      duration: 0.8,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: el,
-        start: "top 85%",
-        toggleActions: "play none none none",
-        once: true,
-      },
-    });
-  });
-
-  ScrollTrigger.refresh();
-});
 
 
 
